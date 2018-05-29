89619635fa41991b4c377a17d5e2b3b55bf1a532
Alb Flavius Robert <albfla96@gmail.com>
2018-05-29 13:54:09 +0300

    Created The Forms App Project

---
+++ SincreticNanu/SincreticNanu/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SincreticNanu
+{
+    public partial class Form1 : Form
+    {
+        SerialPort mySerialPort = new SerialPort("COM3", 9600);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mySerialPort.Open();
+            timer1.Start();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string s = "[b].";
+            mySerialPort.Write(s);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string s= mySerialPort.ReadExisting();
+
+            String[] temperatura = s.Split('/');
+            if (temperatura[0].Length > 0)
+            {
+                textBox1.Text = temperatura[0];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text != null)
+            {
+                mySerialPort.Write(textBox2.Text);
+            }
+        }
+    }
+}
